a7cf0baa33713a7ade6ae0a97ca4c679ddf44d38
Apple <apple@Apples-MacBook-Pro.local>
2025-06-12 13:33:41 +0530

    userMenu

--- script.js
+++ script.js
@@ -5,3 +5,17 @@
 //   mobileMenuLinks.classList.toggle("active");
 //   mmenu.classList.toggle("ri-close-large-fill");
 // });
+
+let account = document.querySelector("#acco");
+let userDiv = document.querySelector(".userDiv");
+
+account.addEventListener("click", () => {
+  userDiv.classList.toggle("active");
+});
+
+let account2 = document.querySelector("#acco2");
+let userDiv2 = document.querySelector(".userDiv2");
+
+account2.addEventListener("click", () => {
+  userDiv2.classList.toggle("active");
+});
