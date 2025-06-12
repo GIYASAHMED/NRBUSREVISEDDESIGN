7e3c627c389df1be75eff1bcbc0b3daef7db7f12
Apple <apple@Apples-MacBook-Pro.local>
2025-06-12 11:52:44 +0530

    mobile menu

---
+++ script.js
@@ -0,0 +1,7 @@
+let hamMenu = document.querySelector("#hamMenu");
+let mobileMenuLinks = document.querySelector(".mobileMenuLinks");
+let mmenu = document.querySelector(".mmenu");
+hamMenu.addEventListener("click", () => {
+  mobileMenuLinks.classList.toggle("active");
+  mmenu.classList.toggle("ri-close-large-fill");
+});
